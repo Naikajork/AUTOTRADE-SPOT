639a3a8ad915d7cdf8f2482d9c7c25b159e7d547
Naikajork <75772779+Naikajork@users.noreply.github.com>
2021-05-22 20:47:42 +0700

    Update config_prod.py
    Corret error

--- config_prod.py
+++ config_prod.py
@@ -23,6 +23,6 @@ firebaseConfig = {
 
 firebaseClient = Firebase(firebaseConfig)
 auth = firebaseClient.auth()
-user = auth.sign_in_with_email_and_password(os.getenv(FIREBASE_EMAIL_AUTH),os.getenv(FIREBASE_PASSWORD))
+user = auth.sign_in_with_email_and_password(os.getenv("FIREBASE_EMAIL_AUTH"),os.getenv("FIREBASE_PASSWORD"))
 
 i
